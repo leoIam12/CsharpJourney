e2460c89e7216aa4f84beaad5d55c3e45d504549
leoIam12 <leonel.p.d.fernandes17@gmail.com>
2026-05-17 16:26:41 +0100

    feat: complete exercise 03 nested loops for urgency chart using rows and cols.

---
+++ Tutorial_Ex03_UrgencyChart/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Tutorial_Ex_03_UrgencyCHart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----> O diretor do hospital quer um gráfico visual de barras para ver a ocupação das macas. <-----");
+
+            Console.Write("Insira o número de Setores: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Insira o número de Macas por Setor: ");
+            int cols = Convert.ToInt32(Console.ReadLine());
+
+            // '*' table
+            for (int row = 1; row <= rows; row++)
+            {
+                for(int col = 1; col <= cols; col++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.Write("Clique Enter para terminar.....");
+            Console.ReadLine();
+        }
+    }
+}
