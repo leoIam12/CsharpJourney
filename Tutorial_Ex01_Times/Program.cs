6274ef9220ec8b733338511040cbbe7d9df3a223
leoIam12 <leonel.p.d.fernandes17@gmail.com>
2026-05-17 14:52:56 +0100

    Create patientNames and waitingTimes arrays and implement input for-loop

--- Tutorial_Ex01_Times/Program.cs
+++ Tutorial_Ex01_Times/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Tutorial_Ex01_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----> Gerir o tempo de espera de 3 pacientes no Hospital de Lembá. <-----");
+
+            string[] patientNames = new string[3];
+            int[] waitingTimes = new int[3];
+
+            Console.WriteLine("Insira os Nomes dos Pacientes: ");
+
+            for (int i = 0; i < patientNames.Length; i++)
+            {
+                Console.Write(i+1 + " - ");
+                patientNames[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Insira quanto tempo(minuto/s) os Pacientes estão a espera: ");
+
+            for (int i = 0; i < waitingTimes.Length; i++)
+            {
+                Console.Write($"O paciente {patientNames[i]} está esperando á: ");
+                patientNames[i] = Console.ReadLine();
+            }
+
+            Console.Write("Clique Enter para terminar.....");
+            Console.ReadLine();
+
+        }
+    }
+}
