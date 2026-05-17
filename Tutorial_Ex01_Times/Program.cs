7241a3de1004de33218e6742692b790f5ee17a97
leoIam12 <leonel.p.d.fernandes17@gmail.com>
2026-05-17 15:15:31 +0100

    know what the longest(maior) time is and know what the shortest(menor) time is.

--- Tutorial_Ex01_Times/Program.cs
+++ Tutorial_Ex01_Times/Program.cs
@@ -19,13 +19,38 @@ namespace Tutorial_Ex01_Times
                 patientNames[i] = Console.ReadLine();
             }
 
+            int maxTime = 0;
+            int minTime = 0;
             Console.WriteLine("Insira quanto tempo(minuto/s) os Pacientes estão a espera: ");
 
             for (int i = 0; i < waitingTimes.Length; i++)
             {
                 Console.Write($"O paciente {patientNames[i]} está esperando á: ");
-                patientNames[i] = Console.ReadLine();
+                waitingTimes[i] = Convert.ToInt32(Console.ReadLine());
+
+
+                //know what the longest(maior) time is and know what the shortest(menor) time is
+                if (i == 0)
+                {
+                    maxTime = waitingTimes[i];
+                    minTime = waitingTimes[i];
+                }
+                else
+                {
+                    if (waitingTimes[i] > maxTime)
+                    {
+                        maxTime = waitingTimes[i];
+                    }
+
+                    if (waitingTimes[i] < minTime)
+                    {
+                        minTime = waitingTimes[i];
+                    }
+                }   
             }
+
+            Console.WriteLine($"O tempo máximo de espera é: {maxTime} min.\nO tempo mínimo de espera é: {minTime} min.");
+
 
             Console.Write("Clique Enter para terminar.....");
             Console.ReadLine();
