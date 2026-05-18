3d22c0a4772fe85435b168829a757cdd873393c3
leoIam12 <leonel.p.d.fernandes17@gmail.com>
2026-05-18 21:43:55 +0100

    implement screening color menu with switch selection and validColor.

--- Tutorial_Ex06_DataValidation/Program.cs
+++ Tutorial_Ex06_DataValidation/Program.cs
@@ -27,6 +27,44 @@ namespace Tutorial_Ex06_DataValidation
                 }
             }
 
+            //screening color(cor da triagem)
+            string[] screeningColor = { "Vermelho", "Amarelo", "Verde" };
+
+            bool validColor = false;
+
+            while (!validColor)
+            {
+                Console.WriteLine("\n----- Menu de Triagem -----");
+                int i = 0;
+                foreach (string color in screeningColor)
+                {
+                    Console.WriteLine($"{i + 1} - {color}");
+                    i++;
+                }
+
+                Console.Write("Indica o número da cor da triagem: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine("Atendimento imediato!");
+                        validColor = true;
+                        break;
+                    case 2:
+                        Console.WriteLine("Esperar 20min!");
+                        validColor = true;
+                        break;
+                    case 3:
+                        Console.WriteLine("Esperar 40min!");
+                        validColor = true;
+                        break;
+                    default:
+                        Console.WriteLine("Não existe essa cor. Tente novamente!");
+                        break;
+                }
+            }
+
             Console.Write("Clique Enter para terminar.....");
             Console.ReadLine();
         }
