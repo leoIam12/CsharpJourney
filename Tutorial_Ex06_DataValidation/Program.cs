a06b2d0464088555d52ef4d9cf44b1ba10281a88
leoIam12 <leonel.p.d.fernandes17@gmail.com>
2026-05-18 21:32:51 +0100

    add patient age validation loop using while true.

---
+++ Tutorial_Ex06_DataValidation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Tutorial_Ex06_DataValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este exercício vai treinar a tua capacidade de blindar...\n " +
+                              "-----> ...O sistema contra utilizadores distraídos que digitam dados impossíveis. <-----");
+
+
+            while (true)
+            {
+                Console.Write("Insira a idade do Paciente: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                if (age < 0 || age > 120)
+                {
+                    Console.WriteLine("Idade inválida! Tente novamente.");
+                    continue; //para obrigar o ciclo a recomeçar do topo imediatamente, sem avançar.
+                }
+                else
+                {
+                    //Se a idade for válida, quebra o ciclo
+                    break;
+                }
+            }
+
+            Console.Write("Clique Enter para terminar.....");
+            Console.ReadLine();
+        }
+    }
+}
