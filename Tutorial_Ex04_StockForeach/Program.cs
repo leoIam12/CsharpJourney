8300f3559ea1550481e6b8e73aac6dd7f537854a
leoIam12 <leonel.p.d.fernandes17@gmail.com>
2026-05-18 18:34:50 +0100

    Create medicationStocks array and implement input for-loop.

---
+++ Tutorial_Ex04_StockForeach/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO.Pipes;
+
+namespace Tutorial_Ex04_StockForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos gerir o stock da farmácia do Hospital de Trindade usando o ciclo foreach\n " +
+                              "-----> (Que serve para varrer um array do início ao fim de forma mais simples e limpa). <-----");
+
+            int[] medicationStocks = new int[5];
+
+            for (int i = 0; i < medicationStocks.Length; i++)
+            {
+                Console.Write($"Quantidade em Stock do Medicamento {i+1}: ");
+                medicationStocks[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("Clique Enter para terminar.....");
+            Console.ReadLine();
+        }
+    }
+}
