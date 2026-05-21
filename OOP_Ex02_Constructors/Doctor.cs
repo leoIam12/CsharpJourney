623868c18f03d47c9608332e952880a3a1e63dfc
leoIam12 <leonel.p.d.fernandes17@gmail.com>
2026-05-21 22:38:09 +0100

    feat: implement Doctor class with constructor, instantiation logic and input validation.

---
+++ OOP_Ex02_Constructors/Doctor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Specialized;
+
+namespace OOP_Ex02_Constructors
+{
+    class Doctor //Classe
+    {
+        public string Name;
+        public string Specialty;
+
+        public Doctor(string name, string specialty) //Constructor
+        {
+            Name = name;
+            Specialty = specialty;
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"\nOlá, sou o/a  Dr/a. {Name}, especialista em {Specialty}.\n");
+        }
+    }
+}
---
+++ OOP_Ex02_Constructors/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace OOP_Ex02_Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----> Estudo sobre Construtores <-----\n");
+
+            string[] nameDoctor = new string[2];
+            string[] specialtyDoctor = new string[2];
+
+
+            for (int i = 0; i < nameDoctor.Length; i++)
+            {
+                Console.Write($"Insira o nome do/a Doutor/a({i+1}): ");
+                nameDoctor[i] = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(nameDoctor[i]))
+                {
+                    Console.WriteLine($"\n[ERRO]: Espaço do nome vazio. Tente novamente!\n");
+                    i--;
+                    continue;
+                }
+
+            }
+
+            for (int x = 0; x < specialtyDoctor.Length; x++)
+            {
+                Console.Write($"\nInsira a especialidade do/a Doutor/a {nameDoctor[x]}: ");
+                specialtyDoctor[x] = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(specialtyDoctor[x]))
+                {
+                    Console.WriteLine($"\n[ERRO]: Espaço da especialidade vazio. Tente novamente!\n");
+                    x--;
+                    continue;
+                }
+            }
+
+            Doctor doc1 = new Doctor(nameDoctor[0], specialtyDoctor[0]);
+            Doctor doc2 = new Doctor(nameDoctor[1], specialtyDoctor[1]);
+
+            doc1.Introduce();
+            doc2.Introduce();
+
+            Console.Write("\nClique Enter para terminar.....");
+            Console.ReadLine();
+        }
+    }
+}
