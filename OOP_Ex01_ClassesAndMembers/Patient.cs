a353351eb5cfa74b120fcb7d45ec4a9f901ef5a7
leoIam12 <leonel.p.d.fernandes17@gmail.com>
2026-05-21 21:56:55 +0100

    feat: implement Patient class with basic members and input validation.

---
+++ OOP_Ex01_ClassesAndMembers/Patient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OOP_Ex01_ClassesAndMembers
+{
+    class Patient
+    {
+        public string name;
+        public int age;
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"\nPaciente: {name} | Idade: {age} anos.\n");
+        }
+    }
+}
---
+++ OOP_Ex01_ClassesAndMembers/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace OOP_Ex01_ClassesAndMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----> Estudo sobre Classes e Membros <-----\n");
+
+            Patient patient = new Patient();
+
+            while (true)
+            {
+                Console.Write("Insira o seu Nome: ");
+                patient.name = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(patient.name))
+                {
+                    Console.WriteLine($"\n[ERRO]: Espaço do nome vazio. Tente novamente!\n");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            while (true)
+            {
+                Console.Write("Insira a sua Idade: ");
+                string ageInput = Console.ReadLine();
+
+                if (int.TryParse(ageInput, out patient.age))
+                {
+                    if (patient.age < 0 || patient.age > 120)
+                    {
+                        Console.WriteLine($"\n[ERRO]: Esta Idade não está no intervalo permitido(0 a 120 anos). Tente novamente!\n");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\n[ERRO]: Por favor, digite apenas números inteiros. Tente novamente!\n");
+                }
+              
+            }
+
+
+                patient.ShowInfo();
+
+            Console.Write("\nClique Enter para terminar.....");
+            Console.ReadLine();
+        }
+    }
+}
