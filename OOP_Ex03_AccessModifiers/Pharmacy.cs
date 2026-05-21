deed4ad0a42780c9883c32b42a22698ab407b57e
leoIam12 <leonel.p.d.fernandes17@gmail.com>
2026-05-21 22:58:00 +0100

    feat: implement Pharmacy class demonstrating private access modifiers and encapsulation.

---
+++ OOP_Ex03_AccessModifiers/Pharmacy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OOP_Ex03_AccessModifiers
+{
+    class Pharmacy //Classe
+    {
+        private int stock; //Sendo Private, o Main não consegue chamar -> ex: Pharmacy.stock = 500;
+
+        public Pharmacy() //Constructor
+        {
+            stock = 10; //inicializei stock com 10
+        }
+
+        public void SellMedicine() //Method
+        {
+            if (stock > 0)
+            {
+                stock--;
+                Console.WriteLine($"\nMedicamento vendido! Stock atual: {stock}.\n");
+            }
+            else
+            {
+                Console.WriteLine("[ERRO]: Stock esgotado!");
+            }
+        }
+
+    }
+}
---
+++ OOP_Ex03_AccessModifiers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OOP_Ex03_AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----> Estudo sobre Modificador de acesso(private serve para proteger dados) <-----\n");
+
+            Pharmacy myPharmacy = new Pharmacy();
+
+            //myPharmacy.stock = 500; -> Dá erro porque a variavel stock é privada
+
+            myPharmacy.SellMedicine();
+            myPharmacy.SellMedicine();
+            myPharmacy.SellMedicine();
+
+            Console.Write("\nClique Enter para terminar.....");
+            Console.ReadLine();
+        }
+    }
+}
