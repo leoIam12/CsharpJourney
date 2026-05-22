9ebf2871bb81882422c94381a9757f5395cbf14c
leoIam12 <leonel.p.d.fernandes17@gmail.com>
2026-05-22 19:06:24 +0100

    Implement name and initial deposit with input and validation and created the BankAccount class.

---
+++ OOP_Ex04_MultibancoATMsystem/BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace OOP_Ex04_MultibancoATMsystem
+{
+    class BankAccount
+    {
+        public string ownerName;
+        private double balance; 
+
+
+        public BankAccount(string name, double initialDeposit)
+        {
+            ownerName = name;
+            balance = initialDeposit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"\nDéposito de {amount} efetuado com sucesso!\n");
+            balance += amount;
+        }
+
+        public void WithDraw(double amount)
+        {
+            if (amount > balance)
+            {
+                Console.WriteLine($"\n[ERRO]: Saldo insuficiente para levantamento!\n");
+            }
+            else
+            {
+                Console.WriteLine($"\nLevantamento de {amount} efetuado com sucesso!\n");
+                balance -= amount;
+            }
+        }
+
+        public void ShowBalance()
+        {
+            Console.WriteLine($"Titular da Conta: {ownerName} | Saldo: {balance}£.");
+        }
+    }
+}
---
+++ OOP_Ex04_MultibancoATMsystem/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace OOP_Ex04_MultibancoATMsystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----> Foste contratado por um banco para criar o software das caixas automáticas(Multibanco) <-----\n");
+
+            string name;
+            double deposit;
+
+            while (true)
+            {
+                Console.Write("\nInsira o seu Nome: ");
+                name = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    Console.WriteLine($"\n[ERRO]: Espaço do nome vazio. Tente novamente!");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            while (true)
+            {
+                Console.Write("\nInsira o seu Depósito Inicial: ");
+                string depositString = Console.ReadLine();
+
+                if (double.TryParse(depositString, out deposit) && deposit >= 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"\n[ERRO]: Espaço do nome vazio. Tente novamente!");
+                }
+            }
+
+            BankAccount myAccount = new BankAccount(name, deposit);
+
+
+            Console.Write("\nClique Enter para terminar.....");
+            Console.ReadLine();
+        }
+    }
+}
