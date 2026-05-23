8be72dd93125838b4d2f7783b53483c969b9a0f7
leoIam12 <leonel.p.d.fernandes17@gmail.com>
2026-05-23 18:21:40 +0100

    feat: implement interactive reception menu and integrate Room object.
    - Criado menu principal com navegação em ciclo infinito (Console.Clear).
    - Implementado switch-case para gerir operações de Check-In, Check-Out e Status.
    - Integrada a instância do objeto Room no fluxo da consola.

--- OOP_Ex05_HotelManager/Program.cs
+++ OOP_Ex05_HotelManager/Program.cs
@@ -42,6 +42,68 @@ namespace OOP_Ex05_HotelManager
                     Console.WriteLine($"\n[Erro]: Valor incorreto. Tente novamente!\n");
                 }
             }
+
+            Room myRoom = new Room(roomNumber, pricePerNightNumber);
+
+            while (true)
+            {
+                Console.Clear();
+
+                string[] optionMenu = {"1 - Ver Estado do Quarto", "2 - Fazer Check-In", "3 - Fazer Check-Out", "4 - Fechar o Sistema"};
+
+                Console.WriteLine("--- Menu Principal ---\n");
+                foreach (string opt in optionMenu)
+                {
+                    Console.WriteLine(opt);
+                }
+
+                int optionNumber;
+                Console.Write("\nOpcão: ");
+                string optionString = Console.ReadLine();
+
+                if (int.TryParse(optionString, out optionNumber) && optionNumber >= 1 && optionNumber <= 4)
+                {
+                    switch (optionNumber)
+                    {
+                        case 1:
+                            myRoom.ShowStatus();
+                            break;
+                        case 2:
+                            myRoom.CheckIn();
+                            break;
+                        case 3:
+                            int nightNumber;
+                            while (true)
+                            {
+                                Console.Write("\nQuantas noites o cliente ficou: ");
+                                string nightString = Console.ReadLine();
+
+                                if (int.TryParse(nightString, out nightNumber) && nightNumber > 0)
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"\n[Erro]: Número de noites incorreto. Tente novamente!\n");
+                                }
+                            }
+                            myRoom.CheckOut(nightNumber);
+                            break;
+                        case 4:
+                            Environment.Exit(0);
+                            break;
+                    }
+                    Console.Write("Voltar ao Menu Principal....");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine($"\n[Erro]: Opção Incorreta. Tente novamente!\n");
+                    Console.Write("Voltar ao Menu Principal....");
+                    Console.ReadLine();
+                }
+                
+            }
         }
     }
 }
