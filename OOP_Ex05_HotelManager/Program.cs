f1328838825a7964cde7c8b8257aedca009f05dc
leoIam12 <leonel.p.d.fernandes17@gmail.com>
2026-05-23 16:36:50 +0100

    Create Room class with constructor and state management methods.
    - Criada a classe Room com encapsulamento de variáveis (isOccupied e pricePerNight).
    - Adicionado construtor para inicializar o quarto vazio por defeito.
    - Implementada a lógica de CheckIn e CheckOut com prevenção de dupla ocupação e cálculo de preço (totalPay como variável local).
    - Criado o método ShowStatus para exibição do estado em tempo real.

---
+++ OOP_Ex05_HotelManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_Ex05_HotelManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ OOP_Ex05_HotelManager/Room.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace OOP_Ex05_HotelManager
+{
+    class Room //Minha classe quarto
+    {
+        public int roomNumber;
+        private bool isOccupied;
+        private double pricePerNight;
+
+        public Room(int room, double price)
+        {
+            roomNumber = room;
+            pricePerNight = price;
+
+            isOccupied = false;
+        }
+
+        public void CheckIn()
+        {
+            if (isOccupied)
+            {
+                Console.WriteLine($"\n[Erro]: O quarto {roomNumber} encontra-se ocupado!\n");
+            }
+            else
+            {
+                isOccupied = true; //o quarto fica ocupado
+                Console.WriteLine($"\nBem-Vindo!\n");
+            }
+        }
+
+        public void CheckOut(int nights)
+        {
+            if (!isOccupied)
+            {
+                Console.WriteLine($"\n[Erro]: Não podes fazer checkout do quarto {roomNumber} porque está vazio.\n");
+            }
+            else
+            {
+                isOccupied = false; //o quarto fica livre novamente
+
+                double totalPay = (double)nights * pricePerNight;
+                Console.WriteLine($"\nTotal a Pagar: {Math.Round(totalPay, 2)}£.\n");
+            }
+        }
+
+        public void ShowStatus()
+        {
+            Console.WriteLine($"\nNúmero do Quarto: {roomNumber}.");
+            Console.WriteLine($"Preço por Noite: {pricePerNight}£.");
+
+            if (isOccupied)
+            {
+                Console.WriteLine("Estado do Quarto: Ocupado.\n");
+            }
+            else
+            {
+                Console.WriteLine("Estado do Quarto: Livre.\n");
+            }
+        }
+    }
+}
